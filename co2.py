2dc62ff6ae9b13ae6dd1de952d754237fe9be155
Archy de Berker <archy@elementai.com>
2020-02-02 21:14:39 +0000

    Add calculation of transatlantic flight equivalent

--- co2.py
+++ co2.py
@@ -17,5 +17,14 @@ def calculate_co2(distance_in_km):
     co2_grams_per_km = 141.9
     co2_kg_per_km = co2_grams_per_km / 1000
 
-
     return distance_in_km * co2_kg_per_km
+
+
+def calculate_flight_equivalent(co2):
+    """Figures for a transatlantic flight from https://www.carbonindependent.org/22.html"""
+    kg_co2_per_hour = 90
+    hr_per_flight = 6.5  # Reasonable based on my experience
+
+    co2_per_flight = kg_co2_per_hour*hr_per_flight
+
+    return co2 / co2_per_flight
