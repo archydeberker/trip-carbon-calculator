8bf969b5a959cd37b534189d1f23f346cc55523c
Archy de Berker <archy.deberker@gmail.com>
2021-01-07 17:26:27 -0500

    Add Google Analytics

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+from dotenv import load_dotenv
 
 import actions
 from flask import Flask, request, render_template, send_file, after_this_request, session
@@ -9,39 +10,42 @@ import traceback
 import exceptions
 from mapping import google, maps
 
+load_dotenv()
+
 app = Flask(__name__)
 app.secret_key = os.urandom(28)
+GA_TRACKING_ID = os.environ.get("GA_TRACKING_ID")
 
 
-@app.route('/')
+@app.route("/")
 def upload_page():
-    return render_template('file_upload.html')
+    return render_template("file_upload.html", GA_TRACKING_ID=GA_TRACKING_ID)
 
 
 @app.errorhandler(exceptions.InvalidFile)
 def handle_invalid_usage(e):
-    return render_template('file_upload.html', error=e.to_dict())
+    return render_template("file_upload.html", error=e.to_dict(), GA_TRACKING_ID=GA_TRACKING_ID)
 
 
 @app.errorhandler(Exception)
 def handle_unknown_error(e):
     app.logger.warning(traceback.format_exc())
     error = exceptions.UnknownError(str(e), e.args)
-    return render_template('file_upload.html', error=error.to_dict())
+    return render_template("file_upload.html", error=error.to_dict(), GA_TRACKING_ID=GA_TRACKING_ID)
 
 
-@app.route('/base')
+@app.route("/base")
 def base():
-    return render_template('base.html')
+    return render_template("base.html", GA_TRACKING_ID=GA_TRACKING_ID)
 
 
-@app.route('/api/handle-upload', methods=['GET', 'POST'])
+@app.route("/api/handle-upload", methods=["GET", "POST"])
 def handle_upload():
     try:
-        data = request.files['data']
+        data = request.files["data"]
 
         original_df = actions.parse_uploaded_file(data)
-        assert 'count' in original_df
+        assert "count" in original_df
 
         df = google.add_trip_data_to_dataframe(original_df.copy())
         df = google.combine_with_original_dataframe(original_df, df)
@@ -53,22 +57,24 @@ def handle_upload():
         raise e
 
     df = maps.add_coords_to_df(df)
-    session["data"] = df.to_json(orient='records')
+    session["data"] = df.to_json(orient="records")
 
-    return render_template('results.html', total_co2=f"{df['total emissions (kg CO2)'].sum():.2f}",
-                           map=maps.clean_html(maps.plot_3d_map(df).to_html(as_string=True,
-                                                                            iframe_width=800,
-                                                                            iframe_height=800,
-                                                                            notebook_display=False
-                                                                            )))
+    return render_template(
+        "results.html",
+        total_co2=f"{df['total emissions (kg CO2)'].sum():.2f}",
+        map=maps.clean_html(
+            maps.plot_3d_map(df).to_html(as_string=True, iframe_width=800, iframe_height=800, notebook_display=False)
+        ),
+        GA_TRACKING_ID=GA_TRACKING_ID,
+    )
 
 
-@app.route('/api/download-results', methods=['GET'])
+@app.route("/api/download-results", methods=["GET"])
 def download_results():
-    data = session.get('data')
+    data = session.get("data")
     df = pd.read_json(data, dtype=False)
     df = actions.format_data_for_download(df)
-    temp = tempfile.NamedTemporaryFile(suffix='.xls')
+    temp = tempfile.NamedTemporaryFile(suffix=".xls")
     df.to_excel(temp.name, index=False)
 
     @after_this_request
@@ -76,7 +82,7 @@ def download_results():
         temp.close()
         return response
 
-    return send_file(temp.name, as_attachment=True, attachment_filename='processed.xls')
+    return send_file(temp.name, as_attachment=True, attachment_filename="processed.xls")
 
 
 if __name__ == "__main__":
